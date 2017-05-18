e74ba2d2e1e6d7c15b074e2655848c811bedf986
Tony Wu <tonytlwu@gmail.com>
2017-05-18 11:03:02 +0100

    Reparses VueJS objects as JSON

--- js/src/interface.js
+++ js/src/interface.js
@@ -318,7 +318,7 @@ let app = new Vue({
 // Fired when the external save button is clicked
 Fliplet.Widget.onSaveRequest(() => {
   // Send back the result
-  Fliplet.Widget.save(app.result).then(() => {
+  Fliplet.Widget.save(JSON.parse(JSON.stringify(app.result))).then(() => {
     // Tell the UI this widget has finished
     Fliplet.Widget.complete();
   });
